64ecf73e53ed6017d370cd8733a50a4947d9fd23
Rami Awadallah <rami.moh.awadallah@gmail.com>
2017-09-07 17:44:30 +0300

    update

--- src/publish/Helpers/helpers.php
+++ src/publish/Helpers/helpers.php
@@ -1,10 +1,20 @@
 <?php
 
+<?php
+
 if (! function_exists('theme')) {
     function theme($path)
     {
         $config = app('config')->get('cms.theme');
-
-        return url($config['folder'].'/'.$config['active'].'/assets/cms/'.$path);
+        return url('/themes/default/assets/cms/'.$path);
     }
 }
+
+// if (! function_exists('theme')) {
+//     function theme($path)
+//     {
+//         $config = app('config')->get('cms.theme');
+
+//         return url($config['folder'].'/'.$config['active'].'/assets/cms/'.$path);
+//     }
+// }
