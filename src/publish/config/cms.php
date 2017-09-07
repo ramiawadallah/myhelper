1ee1849c695faa3e76390a62a6400f1de2830483
Rami Awadallah <rami.moh.awadallah@gmail.com>
2017-09-07 11:09:44 +0300

    update

---
+++ src/publish/config/cms.php
@@ -0,0 +1,21 @@
+<?php 
+
+return [
+
+
+	'theme' => [
+
+		'folder' => 'themes',
+		'active' => 'default'
+
+	],
+
+	'templates' => [
+
+		'home'          => App\Template\HomeTemplate::class,
+		'blog' 			=> App\Template\BlogTemplate::class,
+		'maintenance'   => App\Template\MaintenanceTemplate::class,
+
+	]
+
+];
