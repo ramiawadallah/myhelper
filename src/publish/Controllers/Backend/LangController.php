461681866dc918d33e1c6d0e95258ea709ee69f3
Rami Awadallah <rami.moh.awadallah@gmail.com>
2017-09-11 15:33:22 +0300

    Add Validator options to all files

--- src/publish/Controllers/Backend/LangController.php
+++ src/publish/Controllers/Backend/LangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Validator;
 use App\Lang;
 
 class LangController extends Controller
--- src/publish/Controllers/Backend/UserController.php
+++ src/publish/Controllers/Backend/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Backend;
 use App\User;
 use Illuminate\Http\Request;
-
+use Validator;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
