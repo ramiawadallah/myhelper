8c54e41d7c94fcd72918a9da68c4c209ad6f7f8c
Rami Awadallah <rami.moh.awadallah@gmail.com>
2017-09-14 14:49:31 +0300

    update data

--- src/publish/Helpers/helpers.php
+++ src/publish/Helpers/helpers.php
@@ -5,7 +5,7 @@ if (! function_exists('theme')) {
     function theme($path)
     {
         $config = app('config')->get('cms.theme');
-
-        return url($config['folder'].'/'.$config['active'].'/assets/cms/'.$path);
+        return url('/themes/default/assets/cms/'.$path);
+        //return url($config['folder'].'/'.$config['active'].'/assets/cms/'.$path);
     }
 }
