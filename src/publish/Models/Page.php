6abd71064d539c04ecb8d42206e7f4348c169f3c
Rami Awadallah <rami.moh.awadallah@gmail.com>
2017-10-05 15:37:41 +0300

    update files

--- src/publish/Models/Page.php
+++ src/publish/Models/Page.php
@@ -7,22 +7,35 @@ use Baum\Node;
 
 class Page extends Node
 {
-	protected $fillable = ['title' , 'name', 'uri', 'content', 'template', 'photo', 'created_by', 'updated_by'];
+    protected $fillable = ['title' , 'name', 'uri', 'content', 'template', 'photo', 'created_by', 'updated_by'];
 
     public function updateOrder($order, $orderPage){
-    	$orderPage = $this->findOrFail($orderPage);
+        $orderPage = $this->findOrFail($orderPage);
 
-    	if($order == 'before'){
-    		$this->moveToLeftOf($orderPage);
-    	}elseif($order == 'after'){
-    		$this->moveToRightOf($orderPage);
-    	}elseif ($order == 'childOf') {
-    		$this->makeChildOf($orderPage);
-    	}
+        if($order == 'before'){
+            $this->moveToLeftOf($orderPage);
+        }elseif($order == 'after'){
+            $this->moveToRightOf($orderPage);
+        }elseif ($order == 'childOf') {
+            $this->makeChildOf($orderPage);
+        }
+    }
+
+    public function getPrettyUriAttribute(){
+        return '/'.ltrim($this->uri, '/');
+    }
+
+    public function getLinkToPaddedTitleAttribute($link){
+        $padding = str_repeat('&nbsp;', $this->depth * 4);
+        return $padding.link_to($link, $this->title);
+    }
+
+    public function getPaddedTitleAttribute(){
+        return str_repeat('&nbsp;', $this->depth * 4).$this->title;
     }
 
     public function setNameAttribute($value){
-    	$this->attributes['name'] = $value ?: null;
+        $this->attributes['name'] = $value ?: null;
     }
 
     public function setPhotoAttribute($value){
